42c8c8d7c9efed934c9563022ae78bf2763b48d0
Gavin Stark <gavin@gstark.com>
2015-03-04 20:43:21 -0500

    Initial commit

---
+++ lita_config.rb
@@ -0,0 +1,19 @@
+Lita.configure do |config|
+  # The name your robot will use.
+  config.robot.name = "Roberto"
+
+  # The locale code for the language to use.
+  # config.robot.locale = :en
+
+  # The severity of messages to log. Options are:
+  # :debug, :info, :warn, :error, :fatal
+  # Messages at the selected level and above will be logged.
+  config.robot.log_level = :info
+
+  # The adapter you want to connect with. Make sure you've added the
+  # appropriate gem to the Gemfile.
+  config.robot.adapter = ENV.fetch("LITA_ADAPTER", "shell")
+
+  config.adapters.slack.token = ENV.fetch("SLACK_TOKEN", "")
+end
+
